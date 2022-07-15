a585ca150620d71467ea179612a23bf3d76f149b
Kalka <k@kalka.io>
2022-07-15 14:51:45 -0400

    commit initiale

---
+++ src/SpeedTest/index.jsx
@@ -0,0 +1,203 @@
+import Speedometer, {
+  Background,
+  Needle,
+  Arc,
+  Progress,
+  Marks,
+  Indicator,
+} from "react-speedometer";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faPaw,
+  faCheckCircle,
+  faSpinner,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { useEffect, useState } from "react";
+
+import { useSpring, useTransition, animated as a } from "react-spring";
+
+const SpeedTest = () => {
+  const [started, setStarted] = useState(false);
+
+  const [active, setActive] = useState(false);
+
+  let activeTest = active;
+
+  const [speed, setSpeed] = useState(0);
+
+  const [status, setStatus] = useState({
+    text: "Click to start",
+    className: "",
+    icon: faCheck,
+    spin: false,
+  });
+
+  const textTransition = useTransition(status, {
+    config: {
+      friction: 10,
+    },
+    from: { opacity: 0, y: 10 },
+    enter: { opacity: 1, y: 0 },
+    leave: { opacity: 0, y: -10, config: { clamp: true } },
+  });
+
+  const handleSpeedTest = () => {
+    setStarted(true);
+  };
+
+  const pingServer = async () => {
+    setStatus({
+      ...status,
+      text: "Pinging server...",
+      icon: faSpinner,
+      spin: true,
+    });
+    const response = await fetch("ping", { method: "HEAD" });
+    if (response.ok) {
+      setStatus({
+        ...status,
+        text: "Pinging server...done",
+        icon: faCheckCircle,
+        spin: false,
+      });
+      setActive(true);
+      setStatus({
+        ...status,
+        text: "Testing speed...",
+        icon: faSpinner,
+        spin: true,
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (started) {
+      setStatus({
+        ...status,
+        text: "Started!",
+        icon: faCheckCircle,
+        className: "text-green-300",
+      });
+      pingServer();
+    }
+  }, [started]);
+
+  useEffect(() => {
+    let currentSpeed = speed;
+    let bytes = 0;
+    const testFile = async () => {
+      const response = await fetch("download");
+      if (response.ok) {
+        const stream = response.body.getReader();
+        const startTime = new Date().getTime();
+        for (let i = 0; i < 1000; i++) {
+          const { done, value } = await stream.read();
+          bytes += value.byteLength;
+          const bps =
+            (value.byteLength * 8) /
+            ((new Date().getTime() - startTime) / 1000).toFixed(2);
+          const kbps = (bps / 1024).toFixed(2);
+          console.log(bytes);
+          currentSpeed = kbps;
+          setSpeed(currentSpeed);
+          console.log(currentSpeed);
+          if (done) {
+            break;
+          }
+        }
+        // cancel the stream once we've finished the sample
+        await stream.cancel();
+        console.log("done!");
+
+        setActive(false);
+
+        setStatus({
+          ...status,
+          text: "Testing speed...done!",
+          icon: faCheckCircle,
+          spin: false,
+          className: "text-green-300",
+        });
+      }
+    };
+    if (active) {
+      testFile();
+    } else {
+      let progress = currentSpeed;
+      const interval = setInterval(() => {
+        console.log(progress);
+        if (Math.floor(progress) > 0) {
+          setSpeed((progress /= 1.05));
+        } else {
+          setSpeed(0);
+          console.log("ready");
+          setStarted(false);
+          setStatus({
+            text: "Click to start",
+            icon: faCheck,
+            spin: false,
+          });
+          clearInterval(interval);
+        }
+      }, 10);
+      return () => clearInterval(interval);
+    }
+  }, [active]);
+
+  return (
+    <>
+      <div className="w-full h-full fixed bg-zinc-900">
+        <div className="absolute w-[800px] bg-zinc-800 rounded-2xl text-center top-4 p-12 text-zinc-300 m-auto left-0 right-0">
+          <p className="text-4xl my-4">very simple speed test uwu</p>
+          <p className="text-sm">
+            i am not responsible for any{" "}
+            <FontAwesomeIcon className="fa-bounce" icon={faPaw} />
+          </p>
+          <div className="flex justify-center my-14">
+            <Speedometer value={speed} max={1000}>
+              <Background />
+              <Arc />
+              <Needle baseOffset={40} />
+              <Progress />
+              <Marks step={100} />
+              <Indicator></Indicator>
+            </Speedometer>
+          </div>
+          <div className="my-12">
+            {textTransition((style, i) => (
+              <a.p
+                className={`text-2xl relative h-0 ${i.className}`}
+                style={style}
+              >
+                {i.icon && (
+                  <FontAwesomeIcon
+                    className={`mx-2 ${i.spin && "fa-spin"}`}
+                    icon={i.icon}
+                  />
+                )}
+                {started ? i.text : "Click to start"}
+              </a.p>
+            ))}
+          </div>
+          <button
+            onClick={handleSpeedTest}
+            className="bg-zinc-700 p-4 rounded-2xl disabled:text-zinc-500"
+            disabled={started}
+          >
+            <FontAwesomeIcon
+              className={`mx-2 ${started && "fa-spin"}`}
+              icon={started ? faSpinner : faPaw}
+            />
+            {started ? "Running..." : "Start Test"}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SpeedTest;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import SpeedTest from "./SpeedTest";
+
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <SpeedTest />
+  </React.StrictMode>
+);
